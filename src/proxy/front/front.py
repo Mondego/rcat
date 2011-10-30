e7e572e9633667d3ff19264dfd6c4a0531bc103e
Arthur Rodrigo S Valadares <arthur00@gmail.com>
2011-10-29 17:43:47 -0700

    First attempt at at the frontend for the Proxy.

---
+++ src/proxy/front/front.py
@@ -0,0 +1,28 @@
+import tornado.ioloop
+import tornado.web
+import time
+
+from tornado.options import define, options
+
+define("port", default=8888, help="run on the given port", type=int)
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write("Hello, world")
+        
+    def post(self):
+        timestamp = time.time()
+        argument = self.get_argument("message",False)
+        print argument
+        #self.write(argument+timestamp)
+
+
+application = tornado.web.Application([
+    (r"/", MainHandler),
+])
+
+if __name__ == "__main__":
+    tornado.options.parse_command_line()
+    application.listen(options.port)
+    tornado.ioloop.IOLoop.instance().start()
+    
