bf5348e93598ff0515e99f1d6e4f3cff64487771
tho deb <gentimouton@gmail.com>
2012-01-12 10:29:32 -0800

    bot tries to post

--- src/client/client.py
+++ src/client/client.py
@@ -6,18 +6,21 @@ Created on Oct 29, 2011
 
 
 import httplib
+import urllib
 
 myURL = 'localhost:8888'
 conn = httplib.HTTPConnection(myURL)
-conn.request('GET', '/')
+params = urllib.urlencode({'body':'hello'})
+print params
+conn.request('POST', '/a/message/new', params)
 res = conn.getresponse()
+
 #I am only interested in 200 OK response, anything else can be ignored
-if res.status != 200:
-    print 'GET faield on ', myURL, ', reason: ', res.reason, ', status: ', res.status
-else:
+if res.status == 200:
     data = res.read()
     allheaders = res.getheaders()
     print 'data: ', data 
     for header in allheaders:
         print '  ', header[0], ':', header[1]
-
+else:
+    print 'POST failed on ', myURL, ', reason: ', res.reason, ', status: ', res.status
