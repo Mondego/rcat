9b7b2f34151f2689101827f7cf5da6459618484b
gentimouton <gentimouton@gmail.com>
2012-02-23 18:44:07 -0800

    spawn 3 bots that send a single msg #2

--- src/client/client.py
+++ src/client/client.py
@@ -1,9 +1,36 @@
 from ws4py.client.threadedclient import WebSocketClient
+from time import sleep
 
+class Spawner():
+    
+    def __init__(self):
+        
+        # TODO: read from config file
+        NUMBOTS = 3
+        DELAY = 1 #DELAY between bot connections, in seconds
+        ADDR = 'http://localhost:9000/ws'
+        FREQ = 20 # number of msg to send per sec
+        DURATION = 5 * 60 # how long the bots should be running for, in seconds
+        
+        # create all the bots
+        self.bots = []
 
-
-class EchoClient(WebSocketClient):
-    # from https://github.com/Lawouach/WebSocket-for-Python
+        for i in range(NUMBOTS):
+            bot = Bot(i, FREQ, DURATION, ADDR)
+            self.bots.append(bot)
+            bot.connect()
+            sleep(DELAY)
+            
+        
+    
+class Bot(WebSocketClient):
+    # WebSocketClient from https://github.com/Lawouach/WebSocket-for-Python
+    def __init__(self, id, f, dur, addr):
+        WebSocketClient.__init__(self, addr)
+        # TODO: send msg at frequency f, and during dur seconds
+ 
+        
+    
     def opened(self):
         print 'send hello from bot'
         self.send('Hello from bot')
@@ -16,10 +43,6 @@ class EchoClient(WebSocketClient):
         self.close()
 
 if __name__ == '__main__':
-    try:
-        ws = EchoClient('http://localhost:9000/ws', protocols=['http-only', 'chat'])
-        ws.connect()
-    except KeyboardInterrupt:
-        ws.close()
+    Spawner()
 
 
