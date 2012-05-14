f2039d113f24301b0942f63b1264014f47a031d5
Arthur Rodrigo S Valadares <arthur00@gmail.com>
2012-05-14 13:03:21 -0700

    Allow zooming

--- src/examples/jigsaw/jigsaw.js
+++ src/examples/jigsaw/jigsaw.js
@@ -5,11 +5,16 @@ var canvas;
 var ctx; // canvas context
 var canvasID = "canJigsaw";
 
+var lastX=400, lastY=300;
+var dragStart,dragged;
+
 var MAIN_IMG_WIDTH = 800;
 var MAIN_IMG_HEIGHT = 600;
 
 var BLOCK_IMG_WIDTH = 400;
 var BLOCK_IMG_HEIGHT = 300;
+
+var scaleFactor = 1.1;
 
 var TOTAL_ROWS = 2;
 var TOTAL_COLUMNS = 2;
@@ -61,7 +66,9 @@ function initDrawing () {
   canvas.onmouseup = handleOnMouseUp;
   canvas.onmouseout = handleOnMouseOut;
   canvas.onmousemove = handleOnMouseMove;
-
+  canvas.addEventListener('DOMMouseScroll',handleScroll,false);
+  canvas.addEventListener('mousewheel',handleScroll,false);
+  trackTransforms(ctx);
   initializeNewGame();
 };
 
@@ -405,6 +412,77 @@ function isFinished() {
   return true;
 }
 
+var zoom = function(clicks){
+	    var pt = ctx.transformedPoint(lastX,lastY);
+	    ctx.translate(pt.x,pt.y);
+	    var factor = Math.pow(scaleFactor,clicks);
+	    ctx.scale(factor,factor);
+	    ctx.translate(-pt.x,-pt.y);
+	    DrawGame();	
+}
+
+var handleScroll = function(evt){
+    var delta = evt.wheelDelta ? evt.wheelDelta/40 : evt.detail ? -evt.detail : 0;
+    if (delta) zoom(delta);
+    return evt.preventDefault() && false;
+};
+
+function trackTransforms(ctx) {
+	var svg = document.createElementNS("http://www.w3.org/2000/svg",'svg');
+	var xform = svg.createSVGMatrix();
+	console.log(xform);
+	ctx.getTransform = function(){ return xform; };
+
+	var savedTransforms = [];
+	var save = ctx.save;
+	ctx.save = function(){
+	    savedTransforms.push(xform.translate(0,0));
+	    return save.call(ctx);
+	};
+	var restore = ctx.restore;
+	ctx.restore = function(){
+	    xform = savedTransforms.pop();
+	    return restore.call(ctx);
+	};
+
+	var scale = ctx.scale;
+	ctx.scale = function(sx,sy){
+	    xform = xform.scaleNonUniform(sx,sy);
+	    return scale.call(ctx,sx,sy);
+	};
+	var rotate = ctx.rotate;
+	ctx.rotate = function(radians){
+	    xform = xform.rotate(radians*180/Math.PI);
+	    return rotate.call(ctx,radians);
+	};
+	var translate = ctx.translate;
+	ctx.translate = function(dx,dy){
+	    xform = xform.translate(dx,dy);
+	    return translate.call(ctx,dx,dy);
+	};
+	var transform = ctx.transform;
+	ctx.transform = function(a,b,c,d,e,f){
+	    var m2 = svg.createSVGMatrix();
+	    m2.a=a; m2.b=b; m2.c=c; m2.d=d; m2.e=e; m2.f=f;
+	    xform = xform.multiply(m2);
+	    return transform.call(ctx,a,b,c,d,e,f);
+	};
+	var setTransform = ctx.setTransform;
+	ctx.setTransform = function(a,b,c,d,e,f){
+	    xform.a = a;
+	    xform.b = b;
+	    xform.c = c;
+	    xform.d = d;
+	    xform.e = e;
+	    xform.f = f;
+	    return setTransform.call(ctx,a,b,c,d,e,f);
+	};
+	var pt  = svg.createSVGPoint();
+	ctx.transformedPoint = function(x,y){
+	    pt.x=x; pt.y=y;
+	    return pt.matrixTransform(xform.inverse());
+	}
+}
 
 
 
