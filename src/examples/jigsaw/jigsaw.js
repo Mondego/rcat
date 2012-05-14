1ae1a007c89f5e968ad52f4c3231051236e707fb
gentimouton <gentimouton@gmail.com>
2012-05-14 15:11:33 -0700

    indentation on zoom

--- src/examples/jigsaw/jigsaw.js
+++ src/examples/jigsaw/jigsaw.js
@@ -66,8 +66,10 @@ function initDrawing () {
   canvas.onmouseup = handleOnMouseUp;
   canvas.onmouseout = handleOnMouseOut;
   canvas.onmousemove = handleOnMouseMove;
-  canvas.addEventListener('DOMMouseScroll',handleScroll,false);
-  canvas.addEventListener('mousewheel',handleScroll,false);
+  canvas.addEventListener('DOMMouseScroll',handleScroll,false); // webkit
+  // browsers
+  canvas.addEventListener('mousewheel',handleScroll,false); // non-webkit
+  // browsers
   trackTransforms(ctx);
   initializeNewGame();
 };
@@ -240,9 +242,9 @@ function ClearGame() {
   }
 };
 
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////
 ///////////////////////////////////////// EVENTS
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////
 
 
 
@@ -325,9 +327,9 @@ function handleOnMouseMove(e) {
   }
 }
 
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////
 ///////////////////////////////////////// HELPER METHODS
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////
 
 function clear(c) {
   c.clearRect(0, 0, canvas.width, canvas.height);
@@ -390,11 +392,9 @@ function GetImageBlockOnEqual(list, x, y) {
 }
 
 
-
 function isFinished() {
 
   var total = TOTAL_PIECES;
-
   for (var i = 0; i < total; i++) {
 
     var img = imageBlockList[i];
@@ -412,76 +412,75 @@ function isFinished() {
   return true;
 }
 
-var zoom = function(clicks){
-	    var pt = ctx.transformedPoint(lastX,lastY);
-	    ctx.translate(pt.x,pt.y);
-	    var factor = Math.pow(scaleFactor,clicks);
-	    ctx.scale(factor,factor);
-	    ctx.translate(-pt.x,-pt.y);
-	    DrawGame();	
+
+//------- zoom from http://phrogz.net/tmp/canvas_zoom_to_cursor.html
+
+function zoom(delta){
+  var pt = ctx.transformedPoint(lastX,lastY);
+  ctx.translate(pt.x,pt.y);
+  var factor = Math.pow(scaleFactor,delta);
+  ctx.scale(factor,factor);
+  ctx.translate(-pt.x,-pt.y);
+  DrawGame();	
 }
 
-var handleScroll = function(evt){
-    var delta = evt.wheelDelta ? evt.wheelDelta/40 : evt.detail ? -evt.detail : 0;
-    if (delta) zoom(delta);
-    return evt.preventDefault() && false;
+function handleScroll (evt){
+  // evt.wheelDelta for non-webkit, evt.detail for webkit
+  var delta = evt.wheelDelta ? evt.wheelDelta/40 : evt.detail ? -evt.detail : 0;
+  if (delta) 
+    zoom(delta);
+  return evt.preventDefault() && false;
 };
 
 function trackTransforms(ctx) {
-	var svg = document.createElementNS("http://www.w3.org/2000/svg",'svg');
-	var xform = svg.createSVGMatrix();
-	console.log(xform);
-	ctx.getTransform = function(){ return xform; };
+  var svg = document.createElementNS("http://www.w3.org/2000/svg",'svg');
+  var xform = svg.createSVGMatrix();
+  ctx.getTransform = function(){ return xform; };
 
-	var savedTransforms = [];
-	var save = ctx.save;
-	ctx.save = function(){
-	    savedTransforms.push(xform.translate(0,0));
-	    return save.call(ctx);
-	};
-	var restore = ctx.restore;
-	ctx.restore = function(){
-	    xform = savedTransforms.pop();
-	    return restore.call(ctx);
-	};
+  var savedTransforms = [];
+  var save = ctx.save;
+  ctx.save = function(){
+    savedTransforms.push(xform.translate(0,0));
+    return save.call(ctx);
+  };
+  var restore = ctx.restore;
+  ctx.restore = function(){
+    xform = savedTransforms.pop();
+    return restore.call(ctx);
+  };
 
-	var scale = ctx.scale;
-	ctx.scale = function(sx,sy){
-	    xform = xform.scaleNonUniform(sx,sy);
-	    return scale.call(ctx,sx,sy);
-	};
-	var rotate = ctx.rotate;
-	ctx.rotate = function(radians){
-	    xform = xform.rotate(radians*180/Math.PI);
-	    return rotate.call(ctx,radians);
-	};
-	var translate = ctx.translate;
-	ctx.translate = function(dx,dy){
-	    xform = xform.translate(dx,dy);
-	    return translate.call(ctx,dx,dy);
-	};
-	var transform = ctx.transform;
-	ctx.transform = function(a,b,c,d,e,f){
-	    var m2 = svg.createSVGMatrix();
-	    m2.a=a; m2.b=b; m2.c=c; m2.d=d; m2.e=e; m2.f=f;
-	    xform = xform.multiply(m2);
-	    return transform.call(ctx,a,b,c,d,e,f);
-	};
-	var setTransform = ctx.setTransform;
-	ctx.setTransform = function(a,b,c,d,e,f){
-	    xform.a = a;
-	    xform.b = b;
-	    xform.c = c;
-	    xform.d = d;
-	    xform.e = e;
-	    xform.f = f;
-	    return setTransform.call(ctx,a,b,c,d,e,f);
-	};
-	var pt  = svg.createSVGPoint();
-	ctx.transformedPoint = function(x,y){
-	    pt.x=x; pt.y=y;
-	    return pt.matrixTransform(xform.inverse());
-	}
+  var scale = ctx.scale;
+  ctx.scale = function(sx,sy){
+    xform = xform.scaleNonUniform(sx,sy);
+    return scale.call(ctx,sx,sy);
+  };
+  var translate = ctx.translate;
+  ctx.translate = function(dx,dy){
+    xform = xform.translate(dx,dy);
+    return translate.call(ctx,dx,dy);
+  };
+  var transform = ctx.transform;
+  ctx.transform = function(a,b,c,d,e,f){
+    var m2 = svg.createSVGMatrix();
+    m2.a=a; m2.b=b; m2.c=c; m2.d=d; m2.e=e; m2.f=f;
+    xform = xform.multiply(m2);
+    return transform.call(ctx,a,b,c,d,e,f);
+  };
+  var setTransform = ctx.setTransform;
+  ctx.setTransform = function(a,b,c,d,e,f){
+    xform.a = a;
+    xform.b = b;
+    xform.c = c;
+    xform.d = d;
+    xform.e = e;
+    xform.f = f;
+    return setTransform.call(ctx,a,b,c,d,e,f);
+  };
+  var pt  = svg.createSVGPoint();
+  ctx.transformedPoint = function(x,y){
+    pt.x=x; pt.y=y;
+    return pt.matrixTransform(xform.inverse());
+  }
 }
 
 
