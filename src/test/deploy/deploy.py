f7c9f962ae85cf0b73ecfe9c3d456e33a22332ee
Arthur Rodrigo S Valadares <arthur00@gmail.com>
2012-07-25 15:33:35 -0700

    Small fixes

--- src/test/deploy/deploy.py
+++ src/test/deploy/deploy.py
@@ -121,7 +121,7 @@ def launch_apps(servers):
         if tuples[2]:
             print "Starting app in " + tuples[0]
             cmd = "ssh " + tuples[0] + " \'cp ~/rcat/test/deploy/configs/" + appname + ".cfg.temp ~/rcat/test/" + appname + ".cfg\'"
-            os.system(cmd)       
+            os.system(cmd)
             cmd = "ssh " + tuples[0] + " \'screen -d -m ./rcat/test/run" + appname + ".sh\'"
             print cmd
             os.system(cmd)
