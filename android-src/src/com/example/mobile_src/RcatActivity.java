ec61f6d2317ed0a13816a00a7be1240604146f24
Eugenia Gabrielova <genia.likes.science@gmail.com>
2013-01-16 04:21:25 -0800

    Added template webSocket code.

--- android-src/src/com/example/mobile_src/RcatActivity.java
+++ android-src/src/com/example/mobile_src/RcatActivity.java
@@ -7,6 +7,40 @@ public class RcatActivity extends Activity {
     /**
      * Called when the activity is first created.
      */
+
+    /*
+    List<BasicNameValuePair> extraHeaders = Arrays.asList(
+        new BasicNameValuePair("Cookie", "session=abcd")
+    );
+
+    WebSocketClient client = new WebSocketClient(URI.create("wss://irccloud.com"), new WebSocketClient.Handler() {
+        @Override
+        public void onConnect() {
+            Log.d(TAG, "Connected!");
+        }
+
+        @Override
+        public void onMessage(String message) {
+            Log.d(TAG, String.format("Got string message! %s", message));
+        }
+
+        @Override
+        public void onMessage(byte[] data) {
+            Log.d(TAG, String.format("Got binary message! %s", toHexString(data));
+        }
+
+        @Override
+        public void onDisconnect(int code, String reason) {
+            Log.d(TAG, String.format("Disconnected! Code: %d Reason: %s", code, reason));
+        }
+
+        @Override
+        public void onError(Exception error) {
+            Log.e(TAG, "Error!", error);
+        }
+    }, extraHeaders);
+     */
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
