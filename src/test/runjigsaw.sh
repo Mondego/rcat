3726f6693ebeab6344533e75236cbf515243b573
Arthur Rodrigo S Valadares <arthur00@gmail.com>
2013-01-15 19:09:39 -0800

    Another small change

--- src/test/runjigsaw.sh
+++ src/test/runjigsaw.sh
@@ -2,7 +2,4 @@
 export PYTHONPATH=$PYTHONPATH:~/rcat:~/Dropbox/rcat/src
 cd ~/rcat/test
 
-python ../examples/jigsaw/server/jigsawapp.py
-#python jigsawapp.py -p 20002 -ip opensim.ics.uci.edu
-#python jigsawapp.py -p 20003 -ip opensim.ics.uci.edu
-#python jigsawapp.py -p 20004 -ip opensim.ics.uci.edu
+python ../examples/jigsaw/server/jigsaw.py
