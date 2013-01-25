5bc910cf741342ec47961884fb70d41bec9c09ae
Arthur Rodrigo S Valadares <arthur00@gmail.com>
2013-01-24 16:57:07 -0800

    Fix to runbot: run the correct amount of bots

--- dbg/runbot.sh
+++ dbg/runbot.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export PYTHONPATH=$PYTHONPATH:../src
-for i in $(eval echo {0..$3})
+for i in $(eval echo {1..$3})
 do
 	screen -d -m python ../src/examples/jigsaw/bot/jigsawbot.py -ip $1 -p $2
 	sleep 2
