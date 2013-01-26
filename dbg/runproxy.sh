ce2053d2367ed7cdb75f8ac333b82ad6ce0e8d4f
Arthur Rodrigo S Valadares <arthur00@gmail.com>
2013-01-25 19:23:14 -0800

    Fixed runproxy shell script

--- dbg/runproxy.sh
+++ dbg/runproxy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export PYTHONPATH=$PYTHONPATH:../src
-if [ $1 != 0 ]; then
+if [ "$1" != "" ]; then
 	python ../src/proxy/proxymain.py --port=$1
 else
 	python ../src/proxy/proxymain.py
--- src/test/runproxy.sh
+++ src/test/runproxy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export PYTHONPATH=$PYTHONPATH:../
-if [ $1 != 0 ]; then
+if [ "$1" != "" ]; then
 	python ../proxy/proxymain.py --port=$1
 else
 	python ../proxy/proxymain.py
