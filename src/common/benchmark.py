c2b5db3b6f99a507d8b3bd68c81243ff7dd376e8
Arthur Rodrigo S Valadares <arthur00@gmail.com>
2013-02-22 18:53:57 -0800

    minor fix on benchmark logging header

--- src/common/benchmark.py
+++ src/common/benchmark.py
@@ -17,19 +17,19 @@ class ResourceMonitor():
         self.last_timestamp = 0
         # for each benchmarked value, store its C-struct name, human-readable name, and value at the previous step
         # cf http://linux.die.net/man/2/getrusage  
-        self.benchmarked_values = [['ru_utime', 'userTime', 0], # in seconds
-                                   ['ru_stime', 'systemTime', 0], # in seconds
+        self.benchmarked_values = [['ru_utime', 'userTime', 0],  # in seconds
+                                   ['ru_stime', 'systemTime', 0],  # in seconds
                                    ['ru_maxrss', 'max resident set', 0],
-                                   ['ru_ixrss','shared mem', 0],
-                                   ['ru_idrss','unshared mem', 0],
-                                   ['ru_isrss','unshared stack', 0],
-                                   ['ru_minflt','page faults w/o IO', 0],
+                                   ['ru_ixrss', 'shared mem', 0],
+                                   ['ru_idrss', 'unshared mem', 0],
+                                   ['ru_isrss', 'unshared stack', 0],
+                                   ['ru_minflt', 'page faults w/o IO', 0],
                                    ['ru_majflt', 'page faults with IO', 0],
-                                   ['ru_nswap','swap outs', 0],
-                                   ['ru_inblock','block input ops', 0],
+                                   ['ru_nswap', 'swap outs', 0],
+                                   ['ru_inblock', 'block input ops', 0],
                                    ['ru_oublock', 'block output ops', 0],
-                                   ['ru_nvcsw','v context switches', 0],
-                                   ['ru_nivcsw','iv context switches', 0]]
+                                   ['ru_nvcsw', 'v context switches', 0],
+                                   ['ru_nivcsw', 'iv context switches', 0]]
         if os.path.exists(fname):
             name_split = fname.split('.')
             if len(name_split) > 1:
@@ -47,7 +47,7 @@ class ResourceMonitor():
         self.fname = fname
         self.file = open(self.fname, 'w', 0)
         self.writer = csv.writer(self.file)
-        header = [tup[1] for tup in self.benchmarked_values]
+        header = ['humanTime', 'elapsedTime'] + [tup[1] for tup in self.benchmarked_values]
 #        firstline = ['time', 'user time', 'system time', 'max resident set', 'shared mem', 'unshared mem', 'unshared stack', 'page faults w/ IO', 'page faults w IO',
 #                     'swap outs', 'block input ops', 'block output ops', 'msgs sent', 'msgs received', 'signals rcvd', 'v context switches', 'iv context switches']
         self.writer.writerow(header)
@@ -56,15 +56,15 @@ class ResourceMonitor():
     def collect(self):
         while (self.collecting):
             now = time.time()
-            elapsed_time = now - self.last_timestamp # in seconds
+            elapsed_time = now - self.last_timestamp  # in seconds
             resources = resource.getrusage(resource.RUSAGE_SELF)
-            if self.last_timestamp: # dont log deltas for the first step
+            if self.last_timestamp:  # dont log deltas for the first step
                 bm_values = [getattr(resources, tup[0]) - tup[2] for tup in self.benchmarked_values] 
                 # write values to file
                 self.file = open(self.fname, 'a', 0)
                 self.writer = csv.writer(self.file)
                 usage = []
-                usage.append(time.strftime("%H:%M:%S", time.localtime())) # human-readble time
+                usage.append(time.strftime("%H:%M:%S", time.localtime()))  # human-readble time
                 usage.append(elapsed_time)
                 usage.extend(bm_values)
                 self.writer.writerow(usage)
